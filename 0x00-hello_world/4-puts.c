0bac5eea2c6b95f69e5a7f025c5697f927405709
John Muriithi <muriithijohn634@gmail.com>
2023-06-15 09:19:23 -0700

    wrote a program in the puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main Starting point
+ * puts prints our message
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
