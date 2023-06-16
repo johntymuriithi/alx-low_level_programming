3c6a83a78318bda07c350803c39815514da44f2b
John Muriithi <muriithijohn634@gmail.com>
2023-06-16 06:57:19 -0700

    wrote a program to print ot base 10 numbers and seprate them with a comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
