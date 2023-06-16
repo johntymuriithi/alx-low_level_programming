33261b2f27fd645e4e4ee6d53c5e0ba1abf6bb4c
John Muriithi <muriithijohn634@gmail.com>
2023-06-16 06:43:10 -0700

    wrote a program to print out all base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'a'; d <= 'f'; d++)
+	{
+		putchar(d);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
