d637b9cde74d31eb5606c3167e5a784ce794c8fc
John Muriithi <muriithijohn634@gmail.com>
2023-06-16 05:28:32 -0700

    wrote a program to change a letter to lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 	char d;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                putchar(c);
-        }
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 	for (d = 'A'; d <= 'Z'; d++)
-        {
-                putchar(d);
-        }
+	{
+		putchar(d);
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
