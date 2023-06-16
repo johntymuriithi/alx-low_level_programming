9bdfd5be95e39d4a4382ce72f4bcf9b3ebc93072
John Muriithi <muriithijohn634@gmail.com>
2023-06-16 04:32:16 -0700

    wrote a program to change a letter to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d = 'A';
+	char b = tolower(d);
+
+	putchar(b);
+
+	return (0);
+}
