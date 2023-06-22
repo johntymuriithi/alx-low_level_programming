995b101cf12a93bf5e86fc52383f1f7d7c894ab6
John Muriithi <muriithijohn634@gmail.com>
2023-06-22 04:10:30 -0700

    write a program to multiply two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mul -  multiplies two interger values
+ * @a: value which is the first operand
+ * @b: second operand
+ *
+ * Return: Always product of a and b
+ */
+int mul(int a, int b)
+{
+	return ((a * b));
+}
