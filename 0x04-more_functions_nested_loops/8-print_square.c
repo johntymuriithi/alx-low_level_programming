303f2d37abec79eb43b09eaff33297e245db13ea
John Muriithi <muriithijohn634@gmail.com>
2023-06-22 10:02:57 -0700

    wrote a program to print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - print out squares
+ * @size: size of the square
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
