c03a990707d302eea5926315367d4f203146f4f5
John Muriithi <muriithijohn634@gmail.com>
2023-06-22 07:46:54 -0700

    wrote a program to print out 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - print out numbers in 10 lines
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
