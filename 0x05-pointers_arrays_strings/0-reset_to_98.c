e5a3562260a4129e4a7b52e2caada43e3abe0785
John Muriithi <muriithijohn634@gmail.com>
2023-06-26 04:09:21 -0700

    wrote a program to update variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates variable n value to 98
+ @*n: address to the n variable
+ *
+ * Return : Always return 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
