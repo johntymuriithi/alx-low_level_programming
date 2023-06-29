37a55bc40e8e02192e751980b40781f7eb60181d
John Muriithi <muriithijohn634@gmail.com>
2023-06-29 02:25:32 -0700

    wrote program to caoncatinate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcat - concatinate strings
+ * @dest: pointer to the first string
+ * @src: pointer to the second string
+ *
+ * Return: Return dest variable pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		ptr++;
+		src++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is on More pointers and arrays in C programming language
