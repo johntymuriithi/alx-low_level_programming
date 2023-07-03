2e13b2f0eefe344ff9240440c92f1d79df6a065e
John Muriithi <muriithijohn634@gmail.com>
2023-07-03 05:54:36 -0700

    wrote a program to find a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "main.h"
+/**
+ * _strchr - print the first occuerence of a character
+ * @s: pointer variable
+ * @c: character to compare
+ * Return: return result
+ */
+char *_strchr(char *s, char c)
+{
+	char *ptr = s;
+	char *result = NULL;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == c)
+		{
+			result = ptr;
+			ptr++;
+			break;
+		}
+		ptr++;
+	}
+	return (result);
+}
