4d342c07cd0fd0a3378eb659309c919f39681502
John Muriithi <muriithijohn634@gmail.com>
2023-07-03 06:47:15 -0700

    wrote aprogram to print length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,20 +12,24 @@ unsigned int _strspn(char *s, char *accept)
 	int found = 0;
 	char *p;
 
-	while (*s != '\0') {
+	while (*s != '\0')
+	{
 		found = 0;
-		for (p = accept; *p != '\0'; p++) {
-			if (*s == *p) {
+		for (p = accept; *p != '\0'; p++)
+		{
+			if (*s == *p)
+			{
 				found = 1;
 				break;
 			}
 		}
-		if (!found) {
+		if (!found)
+		{
 			break;
 		}
 		length++;
 		s++;
 	}
 
-	return length;
+	return (length);
 }
