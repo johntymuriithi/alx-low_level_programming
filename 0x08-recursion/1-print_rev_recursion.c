5fa7f10f780f361d10fb8281f0b67988d255e261
John Muriithi <muriithijohn634@gmail.com>
2023-07-05 04:39:01 -0700

    wrote a program to print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: poiner to the first element of the string
+ *
+ * Return: no return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
