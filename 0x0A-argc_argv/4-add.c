fcecfc7fe36b1acb78ecd3302d60074b2d49ec61
John Muriithi <muriithijohn634@gmail.com>
2023-07-07 08:33:16 -0700

    wrote a program to add intergers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - print ou product of two intergers
+ * @argc: number of arguments
+ * @argv: argumets
+ *
+ * Return: Always return 1 in erro
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+
+		else
+		{
+			for (i = 1; i < argc; i++)
+			{
+				char *arg = argv[i];
+
+				for (j = 0; arg[j] != '\0'; j++)
+				{
+					if (!isdigit(arg[j]))
+					{
+						printf("Error\n");
+						return (1);
+					}
+				}
+
+				sum += atoi(arg);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
