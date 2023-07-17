e9014e5c857d2f58820680b611d67abcbc3de357
John Muriithi <muriithijohn634@gmail.com>
2023-07-17 07:59:48 -0700

    wrotea program to creat a struct named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure to the variables
+ * @name: name variable
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: this struture containd all the information pertaing to the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
