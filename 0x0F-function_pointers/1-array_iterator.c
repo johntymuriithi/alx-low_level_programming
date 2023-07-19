ff3be4ee1527e93858d5cdeeacb947b21003d192
John Muriithi <muriithijohn634@gmail.com>
2023-07-19 10:05:09 -0700

    worte a program to print out array of numbers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- * array_iterator - function to call
- * @size: size of the array
- * @array: array of numbers
- * @action: function to call with the array
- *
- * Return: no return
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
+	if (array == NULL || action == NULL)
+		return;
 
-	for (i = 0; i < size; i++)
+	while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
