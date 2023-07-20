5c594f562a7abac00768cbe5bdbbae2f329bbdf8
John Muriithi <muriithijohn634@gmail.com>
2023-07-20 09:21:56 -0700

    wrotea program

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,8 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
+		else
+		{
 		printf("%s", list);
-
+		}
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
