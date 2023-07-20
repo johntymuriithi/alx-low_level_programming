8df397f092445793997cdfeeb44fa28f1ba1f6dd
John Muriithi <muriithijohn634@gmail.com>
2023-07-20 08:04:01 -0700

    wrotea program to print out arguments passed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,11 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
+		printf("%d", va_arg(list, int));
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
-		printf("%d", va_arg(list, int));
 	}
 	printf("\n");
 	va_end(list);
