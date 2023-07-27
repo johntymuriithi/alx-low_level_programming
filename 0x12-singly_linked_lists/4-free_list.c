f80c56b98a9133e12a7fb537a52cc8f2eb13f318
johntymuriithi <muriithijohn634@gmail.com>
2023-07-27 08:31:19 -0700

    wrote a program to free up some memory

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,5 +8,14 @@
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);      
+		current = next_node;
+	}
 }
