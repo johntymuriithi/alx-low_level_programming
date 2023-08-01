83d1973b5c10c8f8f91fcf7d4062cedbebd96c71
johntymuriithi <muriithijohn634@gmail.com>
2023-08-01 10:49:46 -0700

    wrote aprogram to free space in memory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function to freee some spance
+ * @head: nodes
+ *
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *nextnode;
+
+	while (current)
+	{
+		nextnode = current->next;
+		free(current);
+		current = nextnode;
+	}
+	*head = NULL;
+}
