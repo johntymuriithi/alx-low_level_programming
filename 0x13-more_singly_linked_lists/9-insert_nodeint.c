33af417ca263f50cf082e09b1db1a160c01c6816
johntymuriithi <muriithijohn634@gmail.com>
2023-08-01 12:46:09 -0700

    wrote a program

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert at a particula point
+ * @head: head
+ * @idx: index to insert
+ * @n: data
+ *
+ * Return: newnode
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current = *head;
+	unsigned int count = 0;
+	listint_t *newnode;
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+
+	if (idx == 0)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		while (current != NULL && count == idx - 1)
+		{
+			current = current->next;
+			count++;
+		}
+		if (current != NULL)
+		{
+			newnode->next = current->next;
+			current->next = newnode;
+		}
+		else
+		{
+			free(newnode);
+			return (NULL);
+		}
+	}
+	return (newnode);
+}
