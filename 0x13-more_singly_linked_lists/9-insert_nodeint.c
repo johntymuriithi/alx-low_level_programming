c300c20e08cdb1cdfb8e377b5f9996742598cf35
johntymuriithi <muriithijohn634@gmail.com>
2023-08-01 13:04:00 -0700

    wrote a program

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,8 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
-	if (*head == NULL)
+	if (idx == 0)
 	{
+		newnode->next = *head;
 		*head = newnode;
 	}
 	else
