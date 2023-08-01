c234085ce7c87369e8b8ccd8e1964f5efa1f524c
johntymuriithi <muriithijohn634@gmail.com>
2023-08-01 14:08:20 -0700

    wrote a program

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,8 @@ void free_listint2(listint_t **head)
 	listint_t *current = *head;
 	listint_t *nextnode;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
-
 	while (current)
 	{
 		nextnode = current->next;
