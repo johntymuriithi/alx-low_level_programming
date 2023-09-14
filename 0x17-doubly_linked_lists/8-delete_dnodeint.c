c885c8fe949fc1efe63d649e6ab4a2b92eed6bf9
johntymuriithi <muriithijohn634@gmail.com>
2023-09-14 11:34:25 -0700

    files

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+     dlistint_t* current = *head;
+    dlistint_t* next;
+    unsigned int i = 0;
+    
+    if (*head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        next = current->next;
+        if (next != NULL) {
+            next->prev = NULL;
+        }
+        free(current);
+        *head = next;
+        return (1);
+    }
+
+    while (current != NULL)
+    {
+        if (i == index)
+        {
+            next = current->next;
+            if (current->prev != NULL) {
+                current->prev->next = next;
+            }
+            if (next != NULL) {
+                next->prev = current->prev;
+            }
+            free(current);
+            return (1);
+        }
+        current = current->next;
+        i++;
+    }
+
+    return (-1);
+}
+
+
+
+
+
+
+
+
+
