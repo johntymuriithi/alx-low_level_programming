e453f2d77910c6c69043d3bc0c3c4c9dbf0f9e13
johntymuriithi <muriithijohn634@gmail.com>
2023-09-14 05:34:53 -0700

    files

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        int i = 0;
+        while (h != NULL)
+        {
+                i++;
+        }
+
+        return (i);
+}
