d01aa3bc9ceff1ad8eed5eb729f53259cd72ee65
johntymuriithi <muriithijohn634@gmail.com>
2023-09-14 11:48:30 -0700

    files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,8 +18,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			current->prev = newNode;
 		}
-		*h = newNode;
-		return (*h);
+		
+		return (newNode);
 	}
 	while (current != NULL)
 	{
