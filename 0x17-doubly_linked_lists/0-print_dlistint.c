8df8221847d1f8ac0aa1de4f859babbad52f6d26
johntymuriithi <muriithijohn634@gmail.com>
2023-09-14 12:13:16 -0700

    files

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,14 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - print
+ * @h: pointer
+ *
+ * Return: lenthgt
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
+
 	while (h != NULL)
 	{
 		printf("%d", h->n);
