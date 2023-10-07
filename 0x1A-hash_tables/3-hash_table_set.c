5dd9503e16b369004b64f7535f2a5413c42a83ab
johntymuriithi <muriithijohn634@gmail.com>
2023-10-07 07:14:16 -0700

    fils

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,45 +1,47 @@
 #include "hash_tables.h"
 
-
 hash_node_t *create_node(const char *key, const char *value) {
-    hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
+	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (new_node == NULL) {
+		return (NULL);
+	}
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    new_node->next = NULL;
-    return new_node;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	return (new_node);
 }
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
 
-    int index;
-        hash_node_t *current;
-        hash_node_t *new_node;
+	int index;
+	hash_node_t *current;
+	hash_node_t *new_node;
 
-        index = key_index((const unsigned char*)key, ht->size);
-        current = ht->array[index];
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL) {
+		return (0); 
+	}
+	index = key_index((const unsigned char*)key, ht->size);
+	current = ht->array[index];
 
-    while (current != NULL) {
-        if (strcmp(current->key, key) == 0) {
+	while (current != NULL) {
+		if (strcmp(current->key, key) == 0) {
 
-            current->value = strdup(value);
-            if (current->value == NULL) {
-                return (0);
-            }
-            return (1);
-        }
-        current = current->next;
-    }
+			current->value = strdup(value);
+			if (current->value == NULL) {
+				return (0);
+			}
+			return (1); 
+		}
+		current = current->next;
+	}
 
-    new_node = create_node(key, value);
-    if (new_node == NULL) {
-        return (0);
-    }
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node = create_node(key, value);
+	if (new_node == NULL) {
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    return (1);
+	return (1);
 }
