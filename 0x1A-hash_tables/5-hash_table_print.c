618654cca433d9d8ad2f70fc09b6253a3bcf5068
johntymuriithi <muriithijohn634@gmail.com>
2023-10-12 00:10:25 -0700

    files

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,33 +6,27 @@
  *
  * Return: no return
  */
-void hash_table_print(const hash_table_t *ht)
-{
+void hash_table_print(const hash_table_t *ht) {
 	unsigned long int i;
 	hash_node_t *current;
 
-	if (ht == NULL)
-	{
+	if (ht == NULL) {
 		return;
 	}
 
 	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
+	for (i = 0; i < ht->size; i++) {
 		current = ht->array[i];
 
-		while (current != NULL)
-		{
+		while (current != NULL) {
 			printf("'%s': '%s'", current->key, current->value);
-			if (current->next != NULL)
-			{
+			if (current->next != NULL) {
 				printf(", ");
 			}
 			current = current->next;
 		}
 
-		if (i < ht->size - 1 && ht->array[i])
-		{
+		if (i < ht->size - 1 && ht->array[i]) {
 			printf(", ");
 		}
 	}
