8b3c65403328bf5a160584eabd88d0ab699e16ee
johntymuriithi <muriithijohn634@gmail.com>
2023-10-11 23:47:48 -0700

    files

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function to print table
+ * @ht: our table
+ *
+ * Return: no return
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			if (current->next != NULL)
+			{
+				printf(", ");
+			}
+			current = current->next;
+		}
+
+		if (i < ht->size - 1 && ht->array[i])
+		{
+			printf(", ");
+		}
+	}
+	printf("}\n");
+}
