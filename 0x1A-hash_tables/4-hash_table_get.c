50b779566b6a905ac16908d4938298e8e7e0aaf4
johntymuriithi <muriithijohn634@gmail.com>
2023-10-11 23:26:32 -0700

    files

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,21 +1,31 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - function to return the value
+ * @ht: the table
+ * @key: the key
+ *
+ * Return: return the value if found else return null
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
 	hash_node_t *current;
 
-
-	if (ht == NULL || key == NULL || *key == '\0') {
-		return (0); 
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
 	}
-	index = key_index((const unsigned char*)key, ht->size);
+
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
-	if (current != NULL)
+	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
+		{
 			return (current->value);
+		}
+		current = current->next;
 	}
 
 	return (NULL);
