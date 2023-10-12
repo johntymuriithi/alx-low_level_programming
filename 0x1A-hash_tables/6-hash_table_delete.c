97d07432150a41b64f74f9f42211a5629fa77132
johntymuriithi <muriithijohn634@gmail.com>
2023-10-12 00:34:20 -0700

    files

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete item an table
+ * @ht: the table
+ *
+ * Return: no return
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp;
+	hash_node_t *current;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
