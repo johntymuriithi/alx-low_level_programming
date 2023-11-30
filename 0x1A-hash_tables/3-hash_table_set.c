f476c85d5b3e0134970089100aaffa1b3f949d7f
jontymuriithi <muriithijohn634@gmail.com>
2023-11-30 08:37:26 -0800

    solved betty issues on tasked 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,8 +1,18 @@
 #include "hash_tables.h"
+/**
+ * create_node - function to create a node
+ * @key: key of the node
+ * @value: value of the node
+ *
+ * Return: return new_node
+ */
 
-hash_node_t *create_node(const char *key, const char *value) {
+hash_node_t *create_node(const char *key, const char *value)
+{
 	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (new_node == NULL) {
+
+	if (new_node == NULL)
+	{
 		return (NULL);
 	}
 
@@ -12,32 +22,46 @@ hash_node_t *create_node(const char *key, const char *value) {
 	return (new_node);
 }
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
+/**
+ * hash_table_set - function to create the table with nodes
+ * @ht: node
+ * @key: specific key
+ * @value: value of the node
+ *
+ * Return: 1 or 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
 
 	int index;
 	hash_node_t *current;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL) {
-		return (0); 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	{
+		return (0);
 	}
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
-	while (current != NULL) {
-		if (strcmp(current->key, key) == 0) {
-
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
 			current->value = strdup(value);
-			if (current->value == NULL) {
+			if (current->value == NULL)
+			{
 				return (0);
 			}
-			return (1); 
+			return (1);
 		}
 		current = current->next;
 	}
 
 	new_node = create_node(key, value);
-	if (new_node == NULL) {
+	if (new_node == NULL)
+	{
 		return (0);
 	}
 	new_node->next = ht->array[index];
