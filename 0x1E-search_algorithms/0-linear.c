70f4b14f0553e1c93c84c14d4c63c89acc9edc57
johntymuriithi <muriithijohn634@gmail.com>
2024-03-12 22:57:07 +0300

    betty issues solved for now

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,28 @@
 #include "search_algos.h"
 /**
- * @brief 
- * 
- */
-
+* linear_search - function to check fot he index of the value passed
+* @array: the input array
+* @size: size of the array
+* @value: target to check
+*
+* Return: index of the target value
+*/
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+size_t i = 0;
 
-    if (array == NULL)
-        return -1;
+if (array == NULL)
+return (-1);
 
-    while (i < size)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return i;
-        }
-        i++;
-    }
+while (i < size)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+return (i);
+}
+i++;
+}
 
-    return -1;
+return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,52 @@
 #include "search_algos.h"
 /**
- * @brief 
- * 
- */
-
+* binary_search - function to check fot he index of the value passed
+* @array: the input array
+* @size: size of the array
+* @value: target to check
+*
+* Return: index of the target value
+*/
 int binary_search(int *array, size_t size, int value)
 {
-    size_t right = size - 1, left = 0, i, mid;
+size_t right = size - 1, left = 0, i, mid;
 
-    if (array == NULL)
-        return -1;
-        
-    while (left <= right) {
-        mid = left + (right - left) / 2;
+if (array == NULL)
+return (-1);
 
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i == right)
-            {
-                printf("%d", array[i]);
-                break;
-            }
-            printf("%d, ", array[i]);
-        }
-        printf("\n");
+while (left <= right)
+{
+mid = left + (right - left) / 2;
 
-        if (array[mid] == value) {
-            return mid;
-        }
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+if (i == right)
+{
+printf("%d", array[i]);
+break;
+}
+printf("%d, ", array[i]);
+}
+printf("\n");
 
-        
-        if (array[mid] < value) {
-            left = mid + 1;
-        }
-        
-        else {
-            right = mid - 1;
-        }
-    }
+if (array[mid] == value)
+{
+return (mid);
+}
 
-    
-    return -1;
+
+if (array[mid] < value)
+{
+left = mid + 1;
+}
+
+else
+{
+right = mid - 1;
+}
+}
+
+
+return (-1);
 }
